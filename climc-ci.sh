e66575fb573d8d2414a7424d062d27786f9f4b16
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-03-07 11:41:43 +0100

    Change CI

--- climc-ci.sh
+++ climc-ci.sh
@@ -8,16 +8,27 @@ set -e
 set -x
 export TEST_ENV=$PWD/.lispenv
 
-rm -fr $TEST_ENV
-rm -fr ~/quicklisp
-mkdir $TEST_ENV
+cleanup() {
+    rm -fr $TEST_ENV
+    rm -fr ~/quicklisp
+    mkdir $TEST_ENV
+}
 
-cp misc/init.lisp $TEST_ENV
-cd $TEST_ENV
-wget -q http://beta.quicklisp.org/quicklisp.lisp -O quicklisp.lisp
-sbcl --script init.lisp
-ln -s $PWD/.. ./.quicklisp/local-projects/climc
-cd ..
-mkdir -p $HOME/.config
-cp etc/climcrc $HOME/.config/
-sbcl --script climc-ci.lisp
+init() {
+    cp misc/init.lisp $TEST_ENV
+    cd $TEST_ENV
+    wget -q http://beta.quicklisp.org/quicklisp.lisp -O quicklisp.lisp
+    sbcl --script init.lisp
+    ln -s $PWD/.. ./.quicklisp/local-projects/climc
+    cd ..
+}
+
+ci() {
+    mkdir -p $HOME/.config
+    cp etc/climcrc $HOME/.config/
+    sbcl --script climc-ci.lisp
+}
+
+cleanup
+init
+ci
