a9c22908f40a4486365355108bb9b0f566306503
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-03-07 11:05:02 +0100

    Rename script

---
+++ climc-ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Climc tool
+# Copyright (c) 2013 Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+
+set -e
+set -x
+export TEST_ENV=$PWD/.lispenv
+
+rm -fr $TEST_ENV
+rm -fr ~/quicklisp
+mkdir $TEST_ENV
+
+cp misc/init.lisp $TEST_ENV
+cd $TEST_ENV
+wget -q http://beta.quicklisp.org/quicklisp.lisp -O quicklisp.lisp
+sbcl --script init.lisp
+ln -s $PWD/.. ./.quicklisp/local-projects/climc
+cp etc/climcrc $HOME/.config/
+sbcl --script ../test/runtests.lisp
